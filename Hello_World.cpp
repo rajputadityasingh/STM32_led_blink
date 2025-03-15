91fc977323c9e88c350acb57758e4698e08b12cc
rajputadityasingh <work.aditya2023@gmail.com>
2025-03-15 23:07:42 +0530

    init

---
+++ Hello_World.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello_World");
+
+    vector<int> i = {1,2,3,4,5};
+
+    for(auto a : i){
+        printf("i = %i\n",a);
+    }
+
+    return 0;
+}
